0d3c43183ef74a06ec9efdecfb2e4376cc452067
hanenjun <2934966326@qq.com>
2020-03-31 16:12:48 +0800

    初始化文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDom from 'react-dom'
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Main from './containers/main/main'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+ReactDom.render((
+    <HashRouter>
+        <Switch>
+            <Route paht="/login" component={Login}></Route>
+            <Route paht="/register" component={Register}></Route>
+            <Route component={Main}></Route>
+        </Switch>
+    </HashRouter>
+), document.getElementById("root"))
